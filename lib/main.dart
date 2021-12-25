a8f5cff6e0eb854d735c76912ba1b766451c9c64
Art b <94374097+artb433@users.noreply.github.com>
2021-12-25 16:31:47 +0000

    my

--- lib/main.dart
+++ lib/main.dart
@@ -7,19 +7,76 @@ class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Center(
+          child: Column(
+            children: [
               // ignore: deprecated_member_use
-              child: FlatButton(
+              FlatButton(
+                  minWidth: 400,
+                  color: Colors.amber,
                   onPressed: () {
-                    // or as a local variable
                     final player = AudioCache();
-
-                    // call this method when desired
-                    player.play('explosion.mp3');
+                    player.play('note1.wav');
                   },
-                  child: Text('Play Music'))),
+                  child: Text('First note')),
+              // ignore: deprecated_member_use
+              FlatButton(
+                  minWidth: 400,
+                  color: Colors.purple,
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note2.wav');
+                  },
+                  child: Text('Second note')),
+              // ignore: deprecated_member_use
+              FlatButton(
+                  minWidth: 400,
+                  color: Colors.red,
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note3.wav');
+                  },
+                  child: Text('Third note')),
+              // ignore: deprecated_member_use
+              FlatButton(
+                  minWidth: 400,
+                  color: Colors.green,
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note4.wav');
+                  },
+                  child: Text('Fourth note')),
+              // ignore: deprecated_member_use
+              FlatButton(
+                  minWidth: 400,
+                  color: Colors.blue,
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note5.wav');
+                  },
+                  child: Text('Fiveth note')),
+              // ignore: deprecated_member_use
+              FlatButton(
+                  minWidth: 400,
+                  color: Colors.amber,
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note6.wav');
+                  },
+                  child: Text('Sixth note')),
+              // ignore: deprecated_member_use
+              FlatButton(
+                  minWidth: 400,
+                  color: Colors.cyan,
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note7.wav');
+                  },
+                  child: Text('Seventh note')),
+            ],
+          ),
         ),
       ),
     );
