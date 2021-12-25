5c0b23715fac7fd13f520474ed45207763199577
Art b <94374097+artb433@users.noreply.github.com>
2021-12-25 20:59:56 +0000

    my

--- lib/main.dart
+++ lib/main.dart
@@ -9,72 +9,49 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$noteNumber.wav');
   }
 
+  Expanded buttonSpecs({Color? color, int? playNumber, String? text}) {
+    return Expanded(
+      // ignore: deprecated_member_use
+      child: FlatButton(
+          color: color,
+          onPressed: () {
+            playSound(playNumber!);
+          },
+          child: Text(text!)),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('Xylophone by Art.B'),
+        ),
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              // ignore: deprecated_member_use
-              FlatButton(
-                  minWidth: 400,
-                  color: Colors.amber,
-                  onPressed: () {
-                    playSound(1);
-                  },
-                  child: Text('First note')),
-              // ignore: deprecated_member_use
-              FlatButton(
-                  minWidth: 400,
-                  color: Colors.purple,
-                  onPressed: () {
-                    playSound(2);
-                  },
-                  child: Text('Second note')),
-              // ignore: deprecated_member_use
-              FlatButton(
-                  minWidth: 400,
-                  color: Colors.red,
-                  onPressed: () {
-                    playSound(3);
-                  },
-                  child: Text('Third note')),
-              // ignore: deprecated_member_use
-              FlatButton(
-                  minWidth: 400,
-                  color: Colors.green,
-                  onPressed: () {
-                    playSound(4);
-                  },
-                  child: Text('Fourth note')),
-              // ignore: deprecated_member_use
-              FlatButton(
-                  minWidth: 400,
-                  color: Colors.blue,
-                  onPressed: () {
-                    playSound(5);
-                  },
-                  child: Text('Fiveth note')),
-              // ignore: deprecated_member_use
-              FlatButton(
-                  minWidth: 400,
-                  color: Colors.yellow,
-                  onPressed: () {
-                    playSound(6);
-                  },
-                  child: Text('Sixth note')),
-              // ignore: deprecated_member_use
-              FlatButton(
-                  minWidth: 400,
-                  color: Colors.cyan,
-                  onPressed: () {
-                    playSound(7);
-                  },
-                  child: Text('Seventh note')),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(28.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                buttonSpecs(
+                    color: Colors.amber, playNumber: 1, text: 'First note'),
+                buttonSpecs(
+                    color: Colors.pink, playNumber: 2, text: 'Second note'),
+                buttonSpecs(
+                    color: Colors.blue, playNumber: 3, text: 'Third note'),
+                buttonSpecs(
+                    color: Colors.teal, playNumber: 4, text: 'Fourth note'),
+                buttonSpecs(
+                    color: Colors.orange, playNumber: 5, text: 'Fiveth note'),
+                buttonSpecs(
+                    color: Colors.red, playNumber: 6, text: 'Sixth note'),
+                buttonSpecs(
+                    color: Colors.green, playNumber: 7, text: 'Seventh note'),
+              ],
+            ),
           ),
         ),
       ),
